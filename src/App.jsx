7d87d09a67023cd6a7f93c9c536dd8c7a950967a
unknown <brannongarrett77@gmail.com>
2023-08-09 15:48:48 +0200

    Added an 'Add' button

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ export default function App() {
         <label htmlFor="item">New Item</label>
         <input type="text" id="item" />
       </div>
+      <button className="btn">Add</button>
     </form>
   )
 }
